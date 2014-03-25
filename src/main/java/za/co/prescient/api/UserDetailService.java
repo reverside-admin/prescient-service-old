d64798872164355a53397dedbaa8c109117274ab
mrunmaya <mrunmaya.biet@gmail.com>
2014-03-25 10:40:33 +0200

    Log4j implemented

--- src/main/java/za/co/prescient/api/UserDetailService.java
+++ src/main/java/za/co/prescient/api/UserDetailService.java
@@ -1,5 +1,6 @@
 package za.co.prescient.api;
 
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,6 +13,8 @@ import java.util.List;
 @RestController
 @RequestMapping(value = "api/users")
 public class UserDetailService {
+private static final Logger LOGGER = Logger.getLogger(UserDetailService.class);
+
 
     @Autowired
     UserDetailRepository userDetailRepository;
@@ -33,37 +36,45 @@ public class UserDetailService {
 
     @RequestMapping(value = "{userName}/login")
     public UserDetail login(@PathVariable("userName") String userName) {
+        LOGGER.info("Login Service Start");
         UserDetail userDetail = userDetailRepository.findByUserName(userName);
+        LOGGER.info("Login Service End.");
         return userDetail;
     }
 
     @RequestMapping
     public List<UserDetail> get() {
-         return userDetailRepository.findAll();
+        LOGGER.info("Get All UserDetails service");
+        return userDetailRepository.findAll();
     }
 
     @RequestMapping(value = "{userId}")
     public UserDetail get(@PathVariable("userId") Long userId) {
+        LOGGER.info("Get a single UserDetail service");
         return userDetailRepository.findOne(userId);
     }
 
     @RequestMapping(value = "/roles")
     public List<UserType> getUserRoles() {
+        LOGGER.info("Get All Roles service");
         return userTypeRepository.findAll();
     }
 
     @RequestMapping(value = "/status")
     public List<UserStatus> getUserStatus() {
+        LOGGER.info("Get All Status service");
         return userStatusRepository.findAll();
     }
 
     @RequestMapping(value = "/hotel/{hotelId}/departments")
     public Iterable<HotelDepartment> departments(@PathVariable ("hotelId") Long hotelId) {
+        LOGGER.info("Get All Departments by HotelId service");
         return hotelDepartmentRepository.findHotelDepartmentByHotelId(hotelId);
     }
 
     @RequestMapping(value = "/{departmentId}/touchpoints")
     public Iterable<TouchPoint> touchpoints(@PathVariable ("departmentId") Long departmentId) {
+        LOGGER.info("Get All Touch Points by DepartmentId service");
         return touchPointRepository.findTouchPointByHotelDepartmentId(departmentId);
     }
 
