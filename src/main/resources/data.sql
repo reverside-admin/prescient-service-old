bf6b797c4313ea3c4ab66c7a5df7a8bd7e896454
mrunmaya <mrunmaya.biet@gmail.com>
2014-04-14 11:54:18 +0200

    Dummy Guest data inserted for Demo

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -44,25 +44,39 @@ insert into itcs_tag_read (id, guest_card, zone) values (3, 1000, 1);
 insert into itcs_tag_read (id, guest_card, zone) values (4, 1001, 1);
 insert into itcs_tag_read (id, guest_card, zone) values (5, 1002, 1);
 
-insert into itcs_tag_read (id, guest_card, zone) values (6, 21, 2);
-insert into itcs_tag_read (id, guest_card, zone) values (7, 21, 2);
-insert into itcs_tag_read (id, guest_card, zone) values (8, 21, 2);
-insert into itcs_tag_read (id, guest_card, zone) values (9, 22, 2);
-insert into itcs_tag_read (id, guest_card, zone) values (10, 22, 2);
+insert into itcs_tag_read (id, guest_card, zone) values (6, 1003, 2);
+insert into itcs_tag_read (id, guest_card, zone) values (7, 1004, 2);
+insert into itcs_tag_read (id, guest_card, zone) values (8, 1005, 3);
+/*insert into itcs_tag_read (id, guest_card, zone) values (9, 1006, 4);*/
 
 
 insert into guest_profile_detail (id, hotel_id, first_name) values (100, 1, 'guest1');
 insert into guest_profile_detail (id, hotel_id, first_name) values (101, 1, 'guest2');
 insert into guest_profile_detail (id, hotel_id, first_name) values (102, 1, 'guest3');
 
+insert into guest_profile_detail (id, hotel_id, first_name) values (103, 1, 'guest4');
+insert into guest_profile_detail (id, hotel_id, first_name) values (104, 1, 'guest5');
+insert into guest_profile_detail (id, hotel_id, first_name) values (105, 1, 'guest6');
+/*insert into guest_profile_detail (id, hotel_id, first_name) values (106, 1, 'guest7');*/
+
 insert into guest_card_allocation (id, guest_card_id, guest_profile_detail_id) values (1, 1000, 100);
 insert into guest_card_allocation (id, guest_card_id, guest_profile_detail_id) values (2, 1001, 101);
 insert into guest_card_allocation (id, guest_card_id, guest_profile_detail_id) values (3, 1002, 102);
+
+insert into guest_card_allocation (id, guest_card_id, guest_profile_detail_id) values (4, 1003, 103);
+insert into guest_card_allocation (id, guest_card_id, guest_profile_detail_id) values (5, 1004, 104);
+insert into guest_card_allocation (id, guest_card_id, guest_profile_detail_id) values (6, 1005, 105);
+/*insert into guest_card_allocation (id, guest_card_id, guest_profile_detail_id) values (7, 1006, 106);*/
 
 
 insert into guest_stay_detail (id, room_id, guest_id, arrival_time, departure_time, guest_profile_detail_id) values (1, '999', 100, '2010-04-01 00:00:00', '2010-04-05 00:00:00', 100);
 insert into guest_stay_detail (id, room_id, guest_id, arrival_time, departure_time, guest_profile_detail_id) values (2, '888', 101, '2010-04-02 00:00:00', '2010-04-06 00:00:00', 101);
 insert into guest_stay_detail (id, room_id, guest_id, arrival_time, departure_time, guest_profile_detail_id) values (3, '777', 102, '2010-04-03 00:00:00', '2010-04-07 00:00:00', 102);
+
+insert into guest_stay_detail (id, room_id, guest_id, arrival_time, departure_time, guest_profile_detail_id) values (4, '666', 103, '2010-04-03 00:00:00', '2010-04-07 00:00:00', 103);
+insert into guest_stay_detail (id, room_id, guest_id, arrival_time, departure_time, guest_profile_detail_id) values (5, '555', 104, '2010-04-03 00:00:00', '2010-04-07 00:00:00', 104);
+insert into guest_stay_detail (id, room_id, guest_id, arrival_time, departure_time, guest_profile_detail_id) values (6, '444', 105, '2010-04-03 00:00:00', '2010-04-07 00:00:00', 105);
+/*insert into guest_stay_detail (id, room_id, guest_id, arrival_time, departure_time, guest_profile_detail_id) values (7, '333', 106, '2010-04-03 00:00:00', '2010-04-07 00:00:00', 106);*/
 
 
 
